bade8e937d82161211e97fc6525b5a96aa484032
Spiro Spirobel <spirobel@protonmail.com>
2019-08-21 01:36:33 +0200

    add docker start jeykyll script because nix funktioniert

---
+++ docker_serve.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+#docker pull jekyll/jekyll
+docker run --rm -it \
+  --volume="$PWD:/srv/jekyll" \
+  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+  -p 4000:4000 jekyll/jekyll \
+  jekyll serve
